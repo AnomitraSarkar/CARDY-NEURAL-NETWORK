fd8ba9f764652d9ae6324235928ea3666a723e58
Anomitra Sarkar <anomitrasarkar05@gmail.com>
2024-07-01 17:12:57 +0530

    relu

--- cp.py
+++ cp.py
@@ -1,8 +1,34 @@
-inputs = [1,2,3,2.5]
-weights = [
-    [0.2,0.8,-0.5,1.0],
-    [0.5,-0.91,0.26,-0.5],
-    [-0.26,-0.27,0.17,0.87]
-    ]
+# import numpy as np
 
-biases =[2,3,0.5]
+# biases =[2,3,0.5]
+
+
+# def spiral_data(points, classes):
+#     X = np.zeros((points*classes, 2))
+#     y = np.zeros(points*classes, dtype='uint8')
+#     for class_number in range(classes):
+#         ix = range(points*class_number, points*(class_number+1))
+#         r = np.linspace(0.0, 1, points)  # radius
+#         t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+#         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+#         y[ix] = class_number
+#     return X, y
+# import matplotlib.pyplot as plt
+
+# print("Here")
+# X,y = spiral_data(100,12)
+# plt.scatter(X[:,0],X[:,1],c=y,cmap="brg")
+# plt.show()
+
+import math
+
+layer_outputs = [4.8,1.21,2.385]
+
+E = math.e
+
+exp_values = [E**i for i in layer_outputs]
+
+normalize = [i/sum(exp_values) for i in exp_values]
+
+
+print(normalize)
